215c3800961b56e2330239f2815c18a60bee715a
coryrobertson <cory.robertson17@gmail.com>
2023-07-31 17:34:23 -0700

    Updated program icon

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     #[cfg(target_os = "windows")] // conditionally set icon of program on windows
     {
         WindowsResource::new()
-            .set_icon("./assets/program_icon.ico")
+            .set_icon("./assets/program_icon_gimp.ico")
             .compile()?;
     }
     Ok(())
